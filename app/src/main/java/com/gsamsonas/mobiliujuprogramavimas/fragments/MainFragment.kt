bf1700be9c9402dac1214f2ce11098fcf9b5d90f
Gabrielius <gabrielius.samsonas@gmail.com>
2020-09-09 15:57:32 +0300

    add MainFragment.kt to select a homework task to demo

---
+++ app/src/main/java/com/gsamsonas/mobiliujuprogramavimas/fragments/MainFragment.kt
@@ -0,0 +1,36 @@
+package com.gsamsonas.mobiliujuprogramavimas.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import com.gsamsonas.mobiliujuprogramavimas.databinding.FragmentMainBinding
+import com.gsamsonas.mobiliujuprogramavimas.recycleradapters.SelectLabAdapter
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainFragment : Fragment() {
+
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+        setupRecycler(binding.rvLabList)
+        return binding.root
+    }
+
+    private fun setupRecycler(recyclerView: RecyclerView) {
+        recyclerView.adapter = SelectLabAdapter(listOf(
+            Pair("Antras laboratorinis darbas") {
+                findNavController().navigate(MainFragmentDirections.actionMainFragmentToDynamicTextFragment())
+            }
+        ))
+    }
+}
